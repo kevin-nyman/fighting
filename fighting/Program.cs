d390ac4c29a72008068692e457456ee9e0da2d6c
kevin <146670889+kevin-nyman@users.noreply.github.com>
2024-10-11 11:44:31 +0200

    Finished

--- fighting/Program.cs
+++ fighting/Program.cs
@@ -1,21 +1,57 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program {
     static void Main() {
-        Random random = new();
-        Entity entity_1 = new Entity("john", 150, 50);
-        Entity entity_2 = new Entity("pork", 100, 100);
-        entity_1.Attack(entity_2);
+        Console.WriteLine("name your character");
+        string you = Console.ReadLine();
+        
+        Console.Clear();
+        Console.WriteLine("name your opponent");
+        string other = Console.ReadLine();
+
+        EntityManager.AddCharacter(you, 150, 15);
+        EntityManager.AddCharacter(other, 150, 15);
+
+        List<Entity> characters = EntityManager.GetCharacters();
+
+        while (characters[0].hp > 0 && characters[1].hp > 0)
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+            EntityManager.DisplayInfo();
+
+            characters[0].Attack(characters[1]);
+
+            characters[1].Attack(characters[0]); 
+
+            Console.Write("\x1b[47m\x1b[30m");
+            Console.Write("press enter to continue...");
+            Console.WriteLine("\x1b[0m");
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter) {}
+        }
+        Console.Clear();
+        if (characters[0].hp > characters[1].hp){
+            Console.WriteLine($"{characters[0].name} won");
+        }
+        else if (characters[0].hp == characters[1].hp) 
+        {
+            Console.WriteLine("wat");
+        }
+        else {
+            Console.WriteLine($"{characters[1].name} won");
+        }
         Console.ReadLine();
 
     }
 }
 
-class Entity {
-    string name;
-    int hp;
-    int dmg;
+public class Entity {
+    public string name;
+    public int hp;
+    public int dmg;
 
     public Entity(string name, int hp, int dmg) {
         this.name = name;
@@ -24,20 +60,39 @@ class Entity {
     }
 
     public void Attack(Entity opponent) {
-        Console.WriteLine($"{name} attacks {opponent.name} with {opponent.hp} hp for {dmg}");
-        opponent.hp -= dmg;
-        Console.WriteLine($"{opponent.name} now has {opponent.hp} hp");
+        Random random = new Random();
+        int damage = dmg + random.Next(-(int)(dmg * 0.15), (int)(dmg*0.15)+ 1);
+        bool criticalHit = random.Next(1,21) == 1;
+
+        if (criticalHit) {
+            damage *= 2;
+            Console.WriteLine($"{name} lands a critical hit");
+        }
+
+        opponent.hp = Math.Clamp((opponent.hp - damage), 0, opponent.hp);
+
+        Console.WriteLine($"{name} attacks {opponent.name} hp for {damage}");
+        Console.WriteLine($"{opponent.name} now has {opponent.hp} hp\n");
     }
 }
 
-public static class Terminal {
-    public static int index = 0;
-    public static bool updater = false;
-    public static int rows = Console.WindowHeight;
-    public static int cols = Console.WindowWidth;
 
-    public static void Menu() {
-
+public static class EntityManager {
+    public static List<Entity> entities = new List<Entity>();
+    public static void AddCharacter(string name, int hp, int dmg)
+    {
+        entities.Add(new Entity(name, hp, dmg));
     }
 
+
+    public static void DisplayInfo()
+    {
+        foreach (var entity in entities)
+        {
+            Console.WriteLine($"{entity.name} has {entity.hp} hp\n");
+        }
+    }
+    public static List<Entity> GetCharacters() {
+        return entities;
+    }
 }
--- fighting/obj/Debug/net8.0/fighting.AssemblyInfo.cs
+++ fighting/obj/Debug/net8.0/fighting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fighting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69cc9fdb101c44e079c897a76747fa1f5440b4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("fighting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fighting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
